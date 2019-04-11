e29559948ec88ac6d986e8ae7e4f56c83ee0b5b1
Francis Mendez <fmendez884@gmail.com>
2019-04-11 15:11:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def oxford_comma(array)
   if array.length == 1 
     return array.join(',')
@@ -7,4 +8,13 @@ def oxford_comma(array)
     last = array.pop()
     return array.join(', ') + ", and #{last}"
   end
+=======
+def oxford_comma(array)
+  if array.length == 1 
+    return array.join(',')
+  elsif array.length == 2 
+    return array.join(' and ')
+  elsif array.length == 3
+    return array.join(',')
+>>>>>>> 41e65c35a5f4f84903d882c85904a238eb4a86ef
 end
